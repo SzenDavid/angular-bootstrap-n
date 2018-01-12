7eaaa675a1764ed88e00dd35f147bed773db53a7
SzenDavid <david.szenasi@gmail.com>
2018-01-12 09:41:03 +0100

    eee
    elso

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, 
+    AppBootstrapModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
